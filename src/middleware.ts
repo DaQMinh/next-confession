05c060fddf5b4c492bda7efad15e9b65c9ae226e
DaoMinhWysi <114815151+DaQMinh@users.noreply.github.com>
2023-09-12 21:34:44 +0700

    added admin's email

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,16 @@
 // Ref: https://next-auth.js.org/configuration/nextjs#advanced-usage
 import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
+import { env } from "process"
 
 export default withAuth(
     // `withAuth` augments your `Request` with the user's token.
     function middleware(request: NextRequestWithAuth) {
     if (request.nextUrl.pathname.startsWith('/dashboard')) {
         const role = request.nextauth.token?.role
+        const email = request.nextauth.token?.email
         if (role){
-            if (role !== 'Admin' && role !== "Mod") {
+            if (role !== 'Admin' && role !== "Mod" || email !== env.NEXT_PUBLIC_ADMIN_EMAIL) {
                 return NextResponse.rewrite(
                     new URL("/misc/denied", request.url)
                 )
